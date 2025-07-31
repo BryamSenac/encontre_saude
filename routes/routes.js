a5ef2dd5b1f6638ee5b6362d47f09cba45f0a0bb
Ktsu0 <gabrielwag971@gmail.com>
2025-07-31 20:11:01 -0300

    HeaderFuncional

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+export const ROUTES = {
+    home: "/index.html",
+    primeirosSocorros:"/pages/primeiroSocorrosPages/primeirosSocorros.html",
+    sintomas:"/pages/sintomasPages/sintomas.html",
+    prevensao:"/pages/prevensaoPages/prevensão.html",
+    farmacia:"/pages/farmaciasPages/farmacias.html"
+};
