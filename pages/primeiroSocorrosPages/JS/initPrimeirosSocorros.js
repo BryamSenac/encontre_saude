fe9326beb8f1d386027b056a905da1f6fe748e37
Seu Nome <thiagostopassola@gmail.com>
2025-08-04 21:39:40 -0300

    primeiras infos

--- pages/primeiroSocorrosPages/JS/initPrimeirosSocorros.js
+++ pages/primeiroSocorrosPages/JS/initPrimeirosSocorros.js
@@ -4,78 +4,162 @@ export function initPrimeirosSocorros() {
     {
       title: 'Engasgo',
       content: `
-      <p>1. Estimule a vítima a tossir. Se não conseguir, dê até cinco tapas nas costas, entre as escápulas.</p>
-      <p>2. Se não resolver, aplique a <strong>manobra de Heimlich</strong> (compressões abdominais rápidas e firmes).</p>
-      <p>3. Se a vítima perder a consciência, inicie a <em>ressuscitação cardiopulmonar</em> e peça ajuda especializada.</p>
-      <p><small>Informações baseadas no método da Biblioteca Virtual de Saúde e Agência Brasília.</p>`
+      <b><p>1. Engasgamento leve </b>
+      <p> Os primeiros socorros para engasgamento leve são:
+      <p><b>-</b> Pedir para a pessoa tossir 5 vezes com força;
+      <p><b>-</b> Bater 5 vezes no meio das costas, mantendo a mão aberta e num movimento rápido de baixo para cima.
+      <p><b>-</b> Durante o engasgamento leve, a pessoa é capaz de tossir, falar ou chorar, pois as vias aéreas estão parcialmente obstruídas, 
+      e essas medidas podem ajudar a eliminar o objeto ou o alimento preso nas vias aéreas.
+      
+      <b><p>2. Engasgamento grave </b>
+      <p>Os primeiros socorros para engasgamento grave é fazer a manobra de Heimlich, que consiste em:
+      <p><b>-</b> Ficar em pé atrás da vítima, que também deve estar em pé, como mostra o passo 1 da imagem;
+      <p><b>-</b> Passar os braços em volta do tronco da pessoa;
+      <p><b>-</b> Fechar a mão que tem mais força, com o polegar para baixo;
+      <p><b>-</b> Posicionar a mão fechada na região superior do abdômen ou "boca do estômago", que fica entre as costelas, como mostra o passo 2 da imagem;
+      <p><b>-</b> Colocar a outra mão sobre a mão que tem o punho cerrado;
+      <p><b>-</b> Fazer pressão com as mãos contra o estômago da pessoa, para dentro e para cima, como se fosse desenhar uma vírgula, como mostra o passo 3 da imagem.
+      <p><b>-</b> Essa manobra pode ser feita quando a técnica para engasgamento leve não teve resultado ou quando as vias aéreas estão completamente obstruídas, que pode ser identificada quando a pessoa não consegue tossir, falar ou chorar.
+      `
+
     },
     {
       title: 'Massagem Cardíaca',
       content: `
-      <p>1. Cheque os sinais vitais (sem movimentos e sem respirar).</p>
-      <p>2. Ajoelhe-se ao lado da vítima; posicione uma mão sobre a outra no centro do peito (entre os mamilos).</p>
-      <p>3. Com os braços esticados, comprima o tórax cerca de 5 cm, 100 vezes por minuto, com ritmo constante.</p>
-      <p><small>Diretrizes do SAMU 192 orientam esse protocolo.</p>`
+      <p><b> Deve ser realizada quando há uma dor no peito, causando uma sensação de pressão ou aperto, ou ainda quando há dor em outras partes do corpo, como mandíbula, pescoço, costas e barriga.</b>
+      <p>Como proceder:
+      <p><b>-</b> Ajoelhe-se ao lado da pessoa e coloque a palma de uma de suas mãos no centro do peito do paciente.
+      <p><b>-</b> Coloque a palma da outra mão em cima da palma que está no peito e entrelace os dedos.
+      <p><b>-</b> Posicione-se de modo que seus ombros fiquem diretamente acima de suas mãos.
+      <p><b>-</b> Usando seu peso corporal (não apenas seus braços), pressione o peito do indivíduo para baixo. Mantendo as mãos no peito, solte a compressão e permita que ele volte à sua posição original.
+      <p><b>-</b> Repita essa ação em cerca de 100 a 120 vezes por minuto até que uma ambulância chegue ou pelo tempo que puder.
+      `
     },
     {
       title: 'Desmaio',
       content: `
-      <p>1. Afaste a vítima de locais perigosos.</p>
-      <p>2. Deite-a de costas e eleve as pernas acima do nível do coração.</p>
-      <p>3. Afrouxe roupas apertadas e vire a cabeça para o lado.</p>
-      <p>4. Não ofereça água imediatamente. Se não acordar em 1 a 2 min, acione o socorro.</p>
-      <p><small>Procedimentos descritos em cartilha sobre desmaios no Hospital da Luz e outros.</p>`
+      <p><b>Em caso de desmaio, caso a pessoa esteja respirando, os primeiros socorros são:</b>
+      <p><b>-</b> Deitar a pessoa no chão, de barriga para cima, e colocar as pernas mais altas que o corpo e a cabeça, cerca de 30 a 40 centímetros do chão;
+      <p><b>-</b> Afrouxar as roupas e abrir os botões para facilitar a respiração;
+      <p><b>-</b> Ir comunicando com a pessoa, mesmo que ela não responda, referindo que está ali para ajudá-la;
+      <p><b>-</b> Observar possíveis lesões causadas pela queda e se estiver sangrando, parar a hemorragia. 
+      <p><b>-</b> Depois de recuperar do desmaio, pode ser dado 1 saqueta de açúcar, de 5g, diretamente na boca, por baixo da língua.
+      <p><b>-</b> Se a pessoa demorar mais de 1 minuto para acordar, é recomendado chamar uma ambulância através do número 192 e verificar novamente se está respirando, iniciando a massagem cardíaca, caso não esteja.
+      `
     },
     {
       title: 'Convulsão',
       content: `
-      <p>1. Deixe a pessoa no chão, deitada e de lado (posição lateral de segurança).</p>
-      <p>2. Afaste objetos e proteja a cabeça com algo macio.</p>
-      <p>3. Não segure braços ou coloque objetos na boca.</p>
-      <p>4. Se passar de 5 minutos, ou for a 1ª crise, acione ambulância.</p>
-      <p><small>Baseado nos protocolos do Hospital Israelita Albert Einstein.</p>`
+      <p><b> Para realizar os primeiros socorros para convulsão, deve-se:</b>
+      <p><b>-</b> Deitar a pessoa no chão de barriga para cima, para evitar uma queda durante a crise convulsiva;
+      <p><b>-</b> Colocar a pessoa deitada de lado, na posição lateral de segurança, para evitar que possa se engasgar com a própria língua ou com vômito
+      <p><b>-</b> Colocar algo macio sob a cabeça da pessoa, como uma almofada, travesseiro, toalha enrolada ou casaco;
+      <p><b>-</b> Dar espaço para a pessoa, afastando objetos que estejam próximos e que possam causar lesões, como mesas, cadeiras ou objetos pontiagudos ou cortantes;
+      <p><b>-</b> Afrouxar roupas apertadas, se possível, principalmente em volta do pescoço, como camisas ou gravatas;
+      <p><b>-</b> Anotar o horário em que a convulsão começou;
+      <p><b>-</b> Verificar a respiração, quando os movimentos bruscos pararem;
+      <p><b>-</b> Monitorar o nível de consciência e de resposta da pessoa;
+      <p><b>-</b> Anotar o horário em que a convulsão terminou;
+      <p><b>-</b> Tranquilizar a pessoa até que ela se recupere.
+      <p><b>-</b> Se a pessoa não estiver respirando ou não tiver pulso, deve-se iniciar a massagem cardíaca.
+      `
     },
     {
       title: 'Intoxicação',
       content: `
-      <p>1. Acione imediatamente o <strong>Disque‑Intoxicação (0800‑722‑6001)</strong> ou SAMU 192.</p>
-      <p>2. Não provoque vômito, nem ofereça alimentos ou líquidos.</p>
-      <p>3. Lave olhos ou pele exposta com água corrente e sabão.</p>
-      <p>4. Coloque a vítima de lado até o socorro chegar.</p>
-      <p><small>Conforme orientações da ANVISA e Tua Saúde.</p>`
+      <p><b> 1. Primeiros socorros por ingestão </b>
+      <p><b>-</b> Os primeiros socorros após ingestão de produtos, remédios, venenos ou drogas são:
+      <p><b>-</b> Manter a calma e tentar verificar se existem substâncias, frascos ou blister de remédios, drogas ou venenos próximos à vítima;
+      <p><b>-</b> Ligar imediatamente para o SAMU 192 ou corpo de bombeiros no 193, ou levar a vítima imediatamente para o hospital;
+      <p><b>-</b> Entrar em contato com o Centro de Informação e Assistência Toxicológica (CIAT), através do número 0800-722-6001, para receber uma orientação dos profissionais enquanto o socorro médico chega;
+      <p><b>-</b> Colocar a pessoa na posição lateral de segurança, deitada de lado e com a cabeça mais baixa em relação ao corpo;
+      <p><b>-</b> Não induzir o vômito e não oferecer bebidas ou alimentos;
+      <p><b>-</b> Enxaguar ou limpar a boca para remover qualquer substância restante.
+
+      <p><b> 2. Primeiros socorros para intoxicação na pele </b>
+      <p><b>-</b> Os primeiros socorros para intoxicação por contato com a pele são:
+      <p><b>-</b> Lavar o local com água corrente e sabão;
+      <p><b>-</b> Afastar a pessoa da substância tóxica;
+      <p><b>-</b> Tirar imediatamente a roupa e os sapatos contaminados;
+      <p><b>-</b> Ir para o pronto-socorro imediatamente ou ligar para o SAMU no 192 ou corpo de bombeiros no 193;
+      <p><b>-</b> Não dar remédios para a pessoa tomar;
+      <p><b>-</b> Não aplicar substâncias no local afetado, como álcool ou vinagre, por exemplo:
+      <b> No entanto, em alguns casos, como intoxicação por ácido sulfúrico, pode-se aplicar solução de bicarbonato de sódio a 5% no local afetado, após lavar a pele.</b>
+
+      <p><b> 3. Primeiros socorros para intoxicação nos olhos </b>
+      <p><b>-</b> Lavar os olhos imediatamente em água corrente por pelo menos 15 minutos;
+      <p><b>-</b> Não usar uma ducha de alta pressão para lavar o olho;
+      <p><b>-</b> Não aplicar colírios, pomadas oftálmicas, sabonetes ou qualquer outro produto nos olhos;
+      <p><b>-</b> Cobrir o olho afetado com uma gaze estéril ou um pano limpo e seco.
+      <p><b>-</b> Em seguida, deve-se ir imediatamente para o hospital para avaliação com o oftalmologista e realizar o tratamento mais adequado.</p>
+
+      <p><b> 4. Primeiros socorros por inalação </b>
+      <p><b>-</b> Os primeiros socorros para intoxicação por inalação de gases, vapores, fumaça ou monóxido de carbono são:
+      <p><b>-</b> Proteger-se primeiro antes de acudir a vítima;
+      <p><b>-</b> Levar a vítima para um local mais arejado longe do gás ou fumaça;
+      <p><b>-</b> Colocar a vítima na posição lateral de segurança, caso não esteja esteja consciente;
+      <p><b>-</b> Ligar imediatamente para o SAMU 192 ou corpo de bombeiros 193;
+      <p><b>-</b> Iniciar a massagem cardíaca, caso a pessoa não esteja respirando, até a chegada da ambulância.</p>
+      `
     },
     {
       title: 'Afogamento',
       content: `
-      <p>1. Procure ajuda imediatamente (bombeiro 193 ou SAMU 192).</p>
-      <p>2. Forneça à vítima material flutuante (galho, boia, corda). Não entre na água se não souber nadar.</p>
-      <p>3. Fora da água, verifique respiração (método “VOS”). Se não respirar, inicie RCP.</p>
-      <p><small>Baseado no protocolo de afogamento da Vida Saudável e Tua Saúde.</p>`
+      <b> Para realizar os primeiros socorros para convulsão, deve-se:</b>
+      <p><b>-</b> Pedir ajuda para outra pessoa que esteja próxima ao local, para que ambas possam seguir com o socorro;
+      <p><b>-</b> Ligar imediatamente para a ambulância dos bombeiros no 193, caso não seja possível, deve-se ligar para o SAMU no 192;
+      <p><b>-</b> Fornecer algum material flutuante para a pessoa que está se afogando, como garrafas de plástico vazias, pranchas de surf ou materiais de isopor ou de espumas;
+      <p><b>-</b> Tentar realizar o socorro sem entrar na água. Caso a pessoa se encontre a menos de 4 metros de distância, é possível estender um galho ou cabo de vassoura, entretanto, se a vítima estiver a mais de 4 metros de distância, pode-se jogar uma boia com uma corda, segurando na extremidade oposta. Quando a vítima está próxima, é importante oferecer sempre o pé ao invés da mão, pois com o nervosismo, a vítima pode puxar o socorrista para dentro da água;
+      <p><b>-</b> Entrar na água somente se souber nadar;
+      <p><b>-</b> Caso a pessoa seja retirada da água, é importante verificar a respiração, durante 10 segundos, observando os movimentos do tórax e escutando o ar sair pelo nariz. Se estiver respirando, é importante deixar a pessoa deitada de lado até que os profissionais cheguem no local;
+      <p><b>-</b> Se a pessoa estiver consciente, sem sintomas, somente com tosse seca ou com a presença de espuma na boca, é recomendado observar a respiração, pedindo a pessoa para deitar de lado, mantendo-a aquecida, relaxada e calma até a chegada dos bombeiros ou SAMU;
+      <p><b>-</b> Se estiver inconsciente e não estiver respirando, é recomendado iniciar a massagem cardíaca na vítima.</p>
+      `
     },
     {
       title: 'Queimadura',
       content: `
-      <p>1. Resfrie a área afetada com água corrente em temperatura ambiente por 10‑15 min.</p>
-      <p>2. Afrouxe roupas e joias da região queimada.</p>
-      <p>3. Cubra com pano limpo umedecido; não aplique manteiga, pasta ou óleo.</p>
-      <p>4. Queimaduras extensas, químicas, elétricas ou nas mãos/genitais exigem atendimento imediato.</p>
-      <p><small>Seguindo recomendações da Tua Saúde e Biblioteca Virtual do Ministério da Saúde.</p>`
+      <p><b> O que fazer na queimadura de 1º grau:</b>
+      <p><b>-</b> Colocar a região queimada debaixo de água fria por, pelo menos, 15 minutos;
+      <p><b>-</b> Manter um pano limpo e umedecido em água fria na região durante as primeiras 24 horas, trocando sempre que a água aquecer;
+      <p><b>-</b> Não aplicar qualquer produto, como óleo ou manteiga, na queimadura;
+      <p><b>-</b> Passar uma pomada hidratante ou cicatrizante para queimaduras, como Nebacetin ou Unguento </p>
+
+      <p><b> O que fazer na queimadura de 2º grau:</b>
+      <p><b>-</b> Colocar o local afetado debaixo de água corrente fria por, pelo menos, 15 minutos;
+      <p><b>-</b> Lavar cuidadosamente a queimadura com água fria e sabão de pH neutro, evitando esfregar com muita força;
+      <p><b>-</b> Cobrir a região com uma gaze molhada ou com bastante vaselina, e prender com uma ligadura, durante as primeiras 48 horas, trocando sempre que necessário;
+      <p><b>-</b> Não furar as bolhas e não aplicar qualquer produto no local, para evitar o risco de infecção;
+      <p><b>-</b> Procurar ajuda médica se a bolha for muito grande. </p>
+
+      <b> O que fazer na queimadura de 3º grau </b>
+      <p><b>-</b> Chamar imediatamente uma ambulância, ligando para o 192 ou leve a pessoa rapidamente para o hospital;
+      <p><b>-</b> Esfriar a região queimada com soro fisiológico, ou na sua falta, água da torneira, por cerca de 10 minutos;
+      <p><b>-</b> Colocar cuidadosamente uma gaze esterilizada umedecida em soro fisiológico ou um pano limpo sobre a região afetada, até a chegada da ajuda médica. Caso a região queimada seja muito grande, pode-se enrolar um lençol limpo umedecido em soro fisiológico e que não largue pelos;
+      <p><b>-</b> Não colocar nenhum tipo de produto na região afetada.
+      <p><b>-</b> Em alguns casos, a queimadura de 3º grau pode ser tão grave que provoca falha em vários órgãos. Nestes casos, caso a vítima desmaie e deixe de respirar deve-se iniciar a massagem cardíaca.
+      <p><b>-</b> Uma vez que todas as camadas de pele estão afetadas, os nervos, glândulas, músculos e até órgãos internos podem sofrer lesões graves. Neste tipo de queimadura pode não se sentir dor devido à destruição dos nervos, mas é necessária ajuda médica imediata para evitar complicações graves, assim como infecções.
+       `
     },
     {
       title: 'Transporte de vítimas',
       content: `
-      <p>1. Só movimente se houver mais risco em deixar a pessoa no local.</p>
-      <p>2. Apoie a vítima com um braço por baixo dos joelhos e outro pelas costas (estilo “cadeirinha”).</p>
-      <p>3. Evite movimentos bruscos, e mantenha a cabeça e coluna alinhados.</p>
-      <p><small>Diretrizes gerais de transporte em manuel de primeiros socorros.</p>`
+      <p><b>-</b> Se a vítima estiver consciente, pergunte se ela sente dor em alguma parte do corpo. Caso sim, verifique se há fratura ou corte na região e aplique os procedimentos padrões.
+      <p><b>-</b> Caso não haja dor aparente, com o auxílio de uma tábua ou cobertor, improvise uma maca para transportá-la até o posto de saúde mais próximo. Durante o transporte, mantenha sempre a cabeça da vítima elevada e evite movimentos bruscos ao longo do trajeto.
+      `
     },
     {
       title: 'Ferimentos',
       content: `
-      <p>1. Lave com água e sabão apenas se não houver hemorragia grave.</p>
-      <p>2. Pressione com gaze ou tecido limpo para estancar sangramento.</p>
-      <p>3. Caso a hemorragia persista ou haja corpo estranho, vá ao hospital.</p>
-      <p><small>Baseado em manuais padrão de primeiros socorros (Nível básico).</p>`
+      <b> Para realizar os primeiros socorros para ferimentos, deve-se:</b>
+      <p><b>-</b> Manter o membro afetado em repouso, numa posição natural e confortável;
+      <p><b>-</b> Não pressionar o local da fratura e verificar a cor do membro afetado para verificar se tem circulação sanguínea adequada;
+      <p><b>-</b> Imobilizar as articulações que ficam acima e abaixo da lesão, com o uso de talas. Não havendo talas disponíveis, é possível improvisar com pedaços de papelão, revistas ou jornais dobrados ou pedaços de madeira, que devem ser acolchoadas com panos limpos e amarrados ao redor da articulação;
+      <p><b>-</b> Lavar a região com soro fisiológico 0,9%, antes de imobilizar, no caso de fratura exposta;
+      <p><b>-</b> Cobrir o ferimento, no caso da fratura exposta, de preferência com gaze esterilizada ou um pano limpo. Se houver um sangramento muito intenso, é necessário fazer compressão acima da região fraturada para tentar impedir a saída do sangue. Saiba mais detalhes dos primeiros socorros em caso de fratura exposta;
+      <p><b>-</b> Nunca tentar endireitar uma fratura ou colocar o osso no lugar;
+      <p><b>-</b> Não movimentar a vítima, até que o membro esteja seguro e o socorro chegue, a não ser que a pessoa tenha parada cardiorrespiratória. Nesses casos, deve-se movimentar a vítima o menos possível e como um todo, e iniciar a massagem cardíaca. Saiba como fazer a massagem cardíaca corretamente;
+      <p><b>-</b> Aguardar o auxílio médico. Caso não seja possível, recomenda-se levar a vítima para o pronto-socorro mais próximo.</p>`
     }
   ];
 
