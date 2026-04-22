12339da6ce447c4ed7d43f87c75a6917051c4f99
Ktsu0 <gabrielwag971@gmail.com>
2026-04-22 20:51:54 -0300

    Conexão com o banco de dados

--- pages/farmacias_pages/farmacias.js
+++ pages/farmacias_pages/farmacias.js
@@ -1,12 +1,11 @@
-
 import { createSidebar } from "./../../shared/sidebar.js";
 import { createMap } from "./js/create_map.js";
 import { carregarBairros } from "./js/create_bairros.js";
 
 // Aguarda o carregamento completo do DOM
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
     createSidebar();
-    createMap();
-    carregarBairros()
+    await createMap();
+    carregarBairros();
 });
